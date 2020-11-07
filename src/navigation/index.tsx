fb964504a49b9f96e51f3188b303805766b02b52
alita-moore <alita.moore805@gmail.com>
2020-11-07 01:42:26 -0600

    adding more packages and basic dev cleaning tools

---
+++ src/navigation/index.tsx
@@ -0,0 +1,9 @@
+import { createBrowserHistory, History } from "history";
+import React from "react";
+import { Router, Route } from "react-router-dom";
+
+type Props = {
+	history: History | null;
+};
+
+const Navigation = ({ history }: Props) => {};
